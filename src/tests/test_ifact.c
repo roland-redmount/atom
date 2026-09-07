642c322bc19eaac357d488a98b0e7cb812a5cdb5
Roland Nilsson <roland@redmount.net>
2026-09-07 11:10:21 +0200

    Add test_ifact

---
+++ src/tests/test_ifact.c
@@ -0,0 +1,33 @@
+#include "kernel/kernel.h"
+#include "library/library.h"
+#include "parser/TermBuilder.h"
+#include "ui/query.h"
+
+#include "testing/testing.h"
+
+
+void testCallIFactService(void)
+{
+	Atom query = CStringToTerm("id x ifact [named ^x name \"foo\"]");
+	MixedTypeRelation * relation = UserQuery(query);
+	size32 nTuples = 0;
+	while(MixedTypeRelationNext(relation))
+		nTuples++;
+	ASSERT_INT32_EQUAL(nTuples, 1)
+
+	FreeMixedTypeRelation(relation);
+	ReleaseFormula(query);
+}
+
+
+int main(int argc, char * argv[])
+{
+	KernelInitialize();
+	LoadLibraries();
+
+	ExecuteTest(testCallIFactService);
+
+	UnloadLibraries();
+	KernelShutdown();
+	TestSummary();
+}
